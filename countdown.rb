0ed1bbc7bef4243abf603fc768ccf9431dd8fa44
Devin Pierce <devinmpierce@gmail.com>
2018-05-09 01:19:18 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,6 +4,14 @@ def countdown(n)
   until (n == 0) do
     puts "#{n} SECOND(S)!"
     n -= 1
+  end
+  'HAPPY NEW YEAR!'
+end
+
+def countdown(n)
+  until (n == 0) do
+    puts "#{n} SECOND(S)!"
+    n -= 1
     sleep(1)
   end
   'HAPPY NEW YEAR!'
